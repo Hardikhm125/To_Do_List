bc839412f6d1323238e954b3886dc8b7253e0e78
Hardik Mehta <hardikhm125@gmail.com>
2023-08-26 18:58:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Task } from "./Task";
 
-function App() {
+function App() {    
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewtask] = useState("");
+
+  const handleChange = (event) => {
+    setNewtask(event.target.value);
+  };
+
+  const addTask = () => { 
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList(task.taskName !== "" ? [...todoList, task]: todoList);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id))
+  }
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="heading">
+    <h1>TO-DO LIST</h1>
     </div>
+    <div className="addTask">
+      <input onChange={handleChange}></input>
+      <button onClick={addTask}>Add Task</button>
+    </div>
+    <div className="list">
+      {
+        todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          );
+        })}
+    </div>
+  </div>
   );
 }
 
